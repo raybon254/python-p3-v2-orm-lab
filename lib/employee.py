ff668eb3885d6fce71aa3eae665f7d69cffcf726
Brian <musyimib449@gmail.com>
2025-06-05 15:51:46 +0300

    orm lab

--- lib/employee.py
+++ lib/employee.py
@@ -133,21 +133,19 @@ class Employee:
 
     @classmethod
     def instance_from_db(cls, row):
-        """Return an Employee object having the attribute values from the table row."""
+        id, year, summary, employee_id = row
 
-        # Check the dictionary for  existing instance using the row's primary key
-        employee = cls.all.get(row[0])
-        if employee:
-            # ensure attributes match row values in case local instance was modified
-            employee.name = row[1]
-            employee.job_title = row[2]
-            employee.department_id = row[3]
+        if id in cls.all:
+            review = cls.all[id]
+            review.year = year
+            review.summary = summary
+            review.employee_id = employee_id
         else:
-            # not in dictionary, create new instance and add to dictionary
-            employee = cls(row[1], row[2], row[3])
-            employee.id = row[0]
-            cls.all[employee.id] = employee
-        return employee
+            review = cls(year, summary, employee_id, id)
+            cls.all[id] = review
+
+        return review
+
 
     @classmethod
     def get_all(cls):
@@ -186,5 +184,8 @@ class Employee:
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
-        """Return list of reviews associated with current employee"""
-        pass
+        from review import Review  # embedded import to avoid circular dependency
+        sql = "SELECT * FROM reviews WHERE employee_id = ?"
+        CURSOR.execute(sql, (self.id,))
+        rows = CURSOR.fetchall()
+        return [Review.instance_from_db(row) for row in rows]
